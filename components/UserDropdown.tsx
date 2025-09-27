11011d0fbe6c08137393cab7c719089ab0724c86
Pema Wangchuk <pemawangchug1986@gmail.com>
2025-09-28 00:31:31 +0600

    updated dropdown menu

---
+++ components/UserDropdown.tsx
@@ -0,0 +1,77 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
+4;
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LogOut } from "lucide-react";
+import NavItems from "@/components/NavItems";
+
+const UserDropdown = () => {
+	const router = useRouter();
+	const handleSignOut = async () => {
+		router.push("/sign-in");
+	};
+
+	const user = { name: "Pema", email: "pema@yahoo.com" };
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button
+					variant='ghost'
+					className='flex items-center gap-3 text-gray-400 hover:text-yellow-500'
+				>
+					<Avatar>
+						<AvatarImage src='hhttps://www.linkedin.com/in/pema-wangchuk-5561aa224/' />
+						<AvatarFallback className='bg-yellow-500 text-yellow-900 text-sm font-bold'>
+							{user.name[0]}
+						</AvatarFallback>
+					</Avatar>
+					<div className='hidden md-flex flex-col items-start'>
+						<span className='text-base font-medium text-gray-400'>
+							{user.name}
+						</span>
+					</div>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className='text-gray-400'>
+				<DropdownMenuLabel className='flex relative items-center gap-3 py-2'>
+					<Avatar>
+						<AvatarImage src='hhttps://www.linkedin.com/in/pema-wangchuk-5561aa224/' />
+						<AvatarFallback className='bg-yellow-500 text-yellow-900 text-sm font-bold'>
+							{user.name}
+						</AvatarFallback>
+					</Avatar>
+					<div className='flex flex-col'>
+						<span className='text-base font-medium text-gray-400'>
+							{user.name}
+						</span>
+						<span className='text-sm text-gray-500'>{user.email}</span>
+					</div>
+				</DropdownMenuLabel>
+
+				<DropdownMenuSeparator className='bg-gray-600' />
+				<DropdownMenuItem
+					onClick={handleSignOut}
+					className='text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer'
+				>
+					<LogOut className='h-4 w-4 mr-2 hidden sm:block' />
+					Logout
+				</DropdownMenuItem>
+				<DropdownMenuSeparator className='hidden sm:block bg-gray-600' />
+				<nav className='sm:hidden'>
+					<NavItems />
+				</nav>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+};
+
+export default UserDropdown;
